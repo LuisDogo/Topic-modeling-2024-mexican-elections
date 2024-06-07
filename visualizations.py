cd9826b91c32a02bc6f28684024db9ac18a6eef8
luisDogo <luisdogoms@gmail.com>
2024-06-07 02:45:17 -0600

    stopword adder

--- visualizations.py
+++ visualizations.py
@@ -1,3 +1,28 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from corpus_reader import text_deb_1, text_deb_2, text_deb_3
+ 
+#TODO create visualizations per debate, per candidate and in general
+
+general_text = text_deb_1 + text_deb_2 + text_deb_3
+
+wordcloud = WordCloud(width = 1600, height = 800,
+                background_color ='white',
+                min_font_size = 10,
+                colormap = "bone",
+                collocations = False
+                ).generate(general_text)
+ 
+# plot the WordCloud image                       
+plt.figure(figsize = (8, 8), facecolor = None)
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.tight_layout(pad = 0)
+ 
+plt.show()
+
+
+
+
